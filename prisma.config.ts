7bbd9673084c9801c27a1e2711fd4596589cb03a
Jose Manuel Camacho Venzor <josecamachojsmith99@gmail.com>
2025-12-08 13:59:45 -0600

    Fix: Eliminar prisma.config.ts incompatible con Prisma v5

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-// prisma.config.ts
-import { defineConfig } from "prisma/config";
-
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-  datasource: {
-    url: "postgresql://postgres:GUgHJBmqYCW1wQZB@aws-0-us-west-2.pooler.supabase.com:5432/postgres",
-  },
-});
