cbe55150d0766248b4d0b1a35e918927dcad7775
Jose Manuel Camacho Venzor <josecamachojsmith99@gmail.com>
2025-12-08 19:32:36 -0600

    Update: Cambios en Prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,14 @@
 // lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient();
+  global.prisma ||
+  new PrismaClient({
+    log: ["query", "error", "warn"],
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
